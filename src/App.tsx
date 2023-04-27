8ac435fc461e20bf5842e111ab731e3b63529156
JoseJorgeST <sarmientoamayajosejorge@gmail.com>
2023-04-27 13:28:30 -0500

    Se agrega el proyecto

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.scss'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div>
+      <h1>CodeArtisam</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
